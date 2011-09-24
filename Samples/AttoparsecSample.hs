5fb4819c792e2cbe60f091183f652780bb279f57
masaedw <masa.edw@gmail.com>
2011-09-24 21:28:35 +0900

    add action and comment

--- Samples/AttoparsecSample.hs
+++ Samples/AttoparsecSample.hs
@@ -1,19 +1,32 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Data.Attoparsec
-import Data.Attoparsec.Char8 hiding (satisfy)
+import qualified Data.Attoparsec as A
 import qualified Data.ByteString as B
+
+data X = Line B.ByteString
+       | QuotedLine B.ByteString
+         deriving (Show, Eq)
 
 mdEx = choice [preLine, normalLine] `sepBy` eol
 
 preLine = do
-  try $ choice [string "\t", string "    "]
-  normalLine
+  q <- try $ choice [string "\t", string "    "]
+  Line l <- normalLine
+  return $ QuotedLine $ B.concat [q, l]
 
-normalLine = satisfy anyChar
-    where anyChar = (`B.notElem` "\r\n")
+normalLine = do
+  l <- A.takeWhile lineChar
+  return $ Line l
+    where lineChar = (`B.notElem` "\r\n")
 
 eol = choice [try (string "\n\r"),
               try (string "\r\n"),
               string "\n",
               string "\r"]
       <?> "end of line"
+
+main = do
+  f <- B.readFile "sample.md"
+  print $ parse mdEx f -- Partial _
+  -- What I expect is [Line "...", Line "...", Line "..."].
+  -- Where is my mistake?
